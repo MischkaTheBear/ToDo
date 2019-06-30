034984c42386644b39d8772003b051ac1c19ed0f
David Kubicka <davidkubicka@gmail.com>
2019-06-29 22:49:48 -0400

    Add Nav Bar + Button and Alert Functionality

--- ToDo/TodoViewController.swift
+++ ToDo/TodoViewController.swift
@@ -31,5 +31,26 @@ class TodoViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
     }
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //actions after click on alert
+            self.listItemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    
+    }
+    
 }
 
